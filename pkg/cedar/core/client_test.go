999771a05c55281f4c2326268891b885b6790cdf
Dylan Sprague <dylan.sprague@oddball.io>
2021-12-20 12:18:22 -0500

    Implement proof of concept for stubbing Swagger client for testing our CEDAR client code

--- pkg/cedar/core/client_test.go
+++ pkg/cedar/core/client_test.go
@@ -2,6 +2,7 @@ package cedarcore
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -9,7 +10,14 @@ import (
 	ld "gopkg.in/launchdarkly/go-server-sdk.v5"
 
 	"github.com/cmsgov/easi-app/pkg/appcontext"
+	"github.com/cmsgov/easi-app/pkg/apperrors"
+	"github.com/cmsgov/easi-app/pkg/cedar/core/gen/client"
+	"github.com/cmsgov/easi-app/pkg/cedar/core/gen/client/system"
+	genmodels "github.com/cmsgov/easi-app/pkg/cedar/core/gen/models"
 	"github.com/cmsgov/easi-app/pkg/models"
+
+	"github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
 )
 
 type ClientTestSuite struct {
@@ -23,6 +31,79 @@ func TestClientTestSuite(t *testing.T) {
 		logger: zap.NewExample(),
 	}
 	suite.Run(t, tests)
+}
+
+/*
+type ClientService interface {
+	SystemDetailAdd(params *SystemDetailAddParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*SystemDetailAddOK, error)
+
+	SystemDetailFindByID(params *SystemDetailFindByIDParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*SystemDetailFindByIDOK, error)
+
+	SystemDetailUpdate(params *SystemDetailUpdateParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*SystemDetailUpdateOK, error)
+
+	SystemSummaryFindByID(params *SystemSummaryFindByIDParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*SystemSummaryFindByIDOK, error)
+
+	SystemSummaryFindList(params *SystemSummaryFindListParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*SystemSummaryFindListOK, error)
+
+	SetTransport(transport runtime.ClientTransport)
+}
+*/
+
+type StubClientService struct {
+}
+
+func (stub *StubClientService) SystemDetailAdd(params *system.SystemDetailAddParams, authInfo runtime.ClientAuthInfoWriter, opts ...system.ClientOption) (*system.SystemDetailAddOK, error) {
+	panic("unimplemented")
+}
+
+func (stub *StubClientService) SystemDetailFindByID(params *system.SystemDetailFindByIDParams, authInfo runtime.ClientAuthInfoWriter, opts ...system.ClientOption) (*system.SystemDetailFindByIDOK, error) {
+	panic("unimplemented")
+}
+
+func (stub *StubClientService) SystemDetailUpdate(params *system.SystemDetailUpdateParams, authInfo runtime.ClientAuthInfoWriter, opts ...system.ClientOption) (*system.SystemDetailUpdateOK, error) {
+	panic("unimplemented")
+}
+
+func (stub *StubClientService) SystemSummaryFindByID(params *system.SystemSummaryFindByIDParams, authInfo runtime.ClientAuthInfoWriter, opts ...system.ClientOption) (*system.SystemSummaryFindByIDOK, error) {
+	return &system.SystemSummaryFindByIDOK{
+		Payload: &genmodels.SystemSummaryResponse{
+			SystemSummary: []*genmodels.SystemSummary{},
+		},
+	}, nil
+}
+
+func (stub *StubClientService) SystemSummaryFindList(params *system.SystemSummaryFindListParams, authInfo runtime.ClientAuthInfoWriter, opts ...system.ClientOption) (*system.SystemSummaryFindListOK, error) {
+	panic("unimplemented")
+}
+
+func (stub *StubClientService) SetTransport(transport runtime.ClientTransport) {
+	panic("unimplemented")
+}
+
+func CreateTestClient() Client {
+	client := Client{
+		cedarCoreEnabled: func(c context.Context) bool {
+			return true
+		},
+		hc:   http.DefaultClient, // maybe mock this?
+		auth: httptransport.BasicAuth("testUser", "testPassword"),
+		sdk: &client.CEDARCoreAPI{
+			System: &StubClientService{},
+		},
+	}
+
+	return client
+}
+
+func (s ClientTestSuite) TestClientWithStub() {
+	ctx := appcontext.WithLogger(context.Background(), s.logger)
+
+	s.Run("GetSystem returns ResourceNotFoundError for nonexistent system", func() {
+		c := CreateTestClient()
+		resp, err := c.GetSystem(ctx, "doesntexist")
+		s.Nil(resp)
+		s.Assertions.IsType(&apperrors.ResourceNotFoundError{}, err)
+	})
 }
 
 func (s ClientTestSuite) TestClient() {
